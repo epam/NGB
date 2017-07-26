1966f398c5c12a90a965b3b4c11899eb175494de
Mikhail Rodichenko <Mikhail_Rodichenko@epam.com>
2017-07-26 20:32:53 +0300

    VCF tracks now have a setting for variants displaying range (#17)
    * VCF tracks now have a setting for variants displaying range
    
    * Default value for 'maximum variants range' changed to 500kBP

--- client/client/dataServices/local/defaultData.js
+++ client/client/dataServices/local/defaultData.js
@@ -186,6 +186,7 @@ export default {
         minBpCount: 50,
         shortenedIntronLength: 15,
         shortenedIntronsMaximumRange: 500000,
+        variantsMaximumRange: 500000,
         showCenterLine: true,
         showSoftClippedBase: true
     }
