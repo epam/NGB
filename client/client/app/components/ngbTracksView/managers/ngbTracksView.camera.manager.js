ede7b6b2420f5d1663c9f6ba618f50a27112b110
Ekaterina Koltsova <Ekaterina_Koltsova@epam.com>
2017-10-31 11:58:35 +0300

    Implement image exporting into vector format (SVG/pdf) #36

--- client/client/app/components/ngbTracksView/managers/ngbTracksView.camera.manager.js
+++ client/client/app/components/ngbTracksView/managers/ngbTracksView.camera.manager.js
@@ -1,5 +1,4 @@
 import {drawingConfiguration} from '../../../../modules/render/core';
-import PIXI from 'pixi.js';
 
 export default class ngbTracksViewBookmarkCamera {
     getTitle;
@@ -10,7 +9,7 @@ export default class ngbTracksViewBookmarkCamera {
         this.getTracks = getTracks;
     }
 
-    saveBrowserView() {
+    saveBrowserView(type) {
         const tracks = this.getTracks();
         if (tracks && tracks.length > 0) {
             requestAnimationFrame(() => {
@@ -34,12 +33,18 @@ export default class ngbTracksViewBookmarkCamera {
                     })
                     .filter(x => x);
 
-                this._downloadView(data);
+                if(type === 'png') {
+                    this._downloadPNGView(data);
+                }
+                else {
+                    this._downloadSVGView(data);
+                }
             });
         }
     }
 
-    _downloadView(data) {
+    _downloadPNGView(data) {
+        debugger;
         if (data && data.length > 0) {
 
             const canvas = this._getPureCanvas(data);
@@ -62,6 +67,70 @@ export default class ngbTracksViewBookmarkCamera {
                 download: `${this.getTitle()}.png`,
                 name: `${this.getTitle()}.png`,
                 href: canvas.toDataURL('image/png').replace('image/png', 'image/octet-stream')
+            }).click();
+        }
+    }
+
+    _downloadSVGView(data) {
+        debugger;
+        if (data && data.length > 0) {
+            const width = this._getCanvasWidth(data);
+            const height = this._getCanvasHeight(data);
+
+            var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+            svg.setAttribute("version", 1.1);
+            svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+            svg.setAttributeNS("http://www.w3.org/2000/xmlns/", "xmlns:xlink", "http://www.w3.org/1999/xlink");
+            svg.setAttribute("width", width);
+            svg.setAttribute("height", height);
+
+            let y = 0;
+            data.forEach(x => {
+                if (x.name) {
+                    y += 15;
+
+                    var svgText = document.createElementNS("http://www.w3.org/2000/svg", "text");
+                    svgText.setAttribute("x", 0);
+                    svgText.setAttribute("y", y - 3);
+                    svgText.setAttribute("font-family", "Roboto, \"Helvetica Neue\", sans-serif");
+                    svgText.setAttribute("font-size", '12px');
+                    svgText.setAttribute("font-style", 'normal');
+                    svgText.appendChild(document.createTextNode(`${x.format} ${x.name}`));
+
+                    svg.appendChild(svgText);
+                }
+                if (x.img) {
+                    var svgImage = document.createElementNS("http://www.w3.org/2000/svg", "image");
+                    svgImage.setAttribute("width", x.img.width);
+                    svgImage.setAttribute("height", x.img.height);
+                    svgImage.setAttribute("x", 0);
+                    svgImage.setAttribute("y", y);
+                    svgImage.setAttributeNS("http://www.w3.org/1999/xlink", "xlink:href", x.img.toDataURL());
+
+                    svg.appendChild(svgImage);
+
+                    y += x.height;
+                }
+            });
+
+            var serializer = new XMLSerializer();
+            var source = serializer.serializeToString(svg);
+
+            if (!source.match(/^<svg[^>]+xmlns="http\:\/\/www\.w3\.org\/2000\/svg"/)) {
+                source = source.replace(/^<svg/, '<svg xmlns="http://www.w3.org/2000/svg"');
+            }
+            if (!source.match(/^<svg[^>]+"http\:\/\/www\.w3\.org\/1999\/xlink"/)) {
+                source = source.replace(/^<svg/, '<svg xmlns:xlink="http://www.w3.org/1999/xlink"');
+            }
+
+            source = '<?xml version="1.0" standalone="no"?>\r\n' + source;
+
+            var url = "data:image/svg+xml;charset=utf-8," + encodeURIComponent(source);
+
+            Object.assign(document.createElement('a'), {
+                download: `${this.getTitle()}.svg`,
+                name: `${this.getTitle()}.svg`,
+                href: url
             }).click();
         }
     }
