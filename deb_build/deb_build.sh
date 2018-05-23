7c253ef77a07765515c7c8baa0a7aab56a70b381
NickPanyushev <panyushev@nextmail.ru>
2018-05-23 14:27:15 +0300

    deb_build bug_fix

--- deb_build/deb_build.sh
+++ deb_build/deb_build.sh
@@ -1,34 +1,29 @@
-#!/bin/bash
-set -e
-#Unpacking files from archives 
-#TODO: Repack files
-#	add dialog for port change
-#	add .deb package size 
+#!/bin/bash -e
 
 tar -xvf pack_files.tar.gz 
-
-#Adding changes description and NGB version to /DEBIAN/changelog file and to /DEBIAN/control - 
-#on Travis-CI
 
 #Adding catgenome.jar and cli
 cp ngb-cli-*.tar.gz ngb/opt/ngb
 cp catgenome-*.jar ngb/opt/ngb
 cd ngb/opt/ngb
 tar -xvf ngb-*.tar.gz
-mv -n ngb-cli/* ./
+mv ngb-cli/bin/ngb bin/
+mv ngb-cli/lib/* lib/
 rm -rf ngb-cli/
 rm ngb-cli-*.tar.gz
-mv catgenome-*.jar bin/
+rm catgenome-*.jar
+
+#mv catgenome-*.jar bin/
 cd ~/NGB/deb_build
 
 #Building the package 
 sudo dpkg-deb --build ngb/
 
 #Installing the package 
-sudo dpkg -i ngb.deb 
+sudo apt install ./ngb.deb 
 
 #Removing the building files
-
+rm -rf ngb
 
 
 
