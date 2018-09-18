ae1439cfbf8c1898090ce8720c3ba733a02ef48b
IanaKabakova <31762018+IanaKabakova@users.noreply.github.com>
2018-09-18 17:40:43 +0300

    Fix issue#191 error displaying reads (#192)
    * fix: Fix display of reads with negative start
    
    * feature: Add comprehensible exception for matches out of reference
    
    * refactor: Add assert, Rename test
    
    * style: Fix according to checkstyle
    
    * test: Aggregate SamRecordHandler tests in one class
    
    * test: Refactor
    
    * fix: Implement index check
    
    * chore: Remove useless parenthesis

---
+++ server/catgenome/src/main/java/com/epam/catgenome/exception/SamAlignmentException.java
@@ -0,0 +1,8 @@
+package com.epam.catgenome.exception;
+
+public class SamAlignmentException extends RuntimeException {
+
+    public SamAlignmentException(String message) {
+        super(message);
+    }
+}
