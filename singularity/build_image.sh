36738c913972ee180f011df987466f52821b1ca9
Aleksandr Sidoruk <aleksandr_sidoruk@epam.com>
2018-12-17 16:13:09 +0300

    NGB Singularity - support both 2.4 and 3.0 (#225)

--- singularity/build_image.sh
+++ singularity/build_image.sh
@@ -1,5 +1,6 @@
 SINGULARITY_FILE=$1
 SINGULARITY_IMAGE=$2
+SINGULARITY_WD=${3:-dist}
 
 if [ -z "$SINGULARITY_FILE" ]; then
   echo "Path to singularity file is not specified, exiting..."
@@ -11,22 +12,14 @@ if [ -z "$SINGULARITY_IMAGE" ]; then
   echo "Resulting image path is not set, using current directory: $SINGULARITY_IMAGE"
 fi
 
-NGB_DOCKER_IMAGE="ngb:latest"
-# Check whether ngb:latest is built
-docker inspect --type=image $NGB_DOCKER_IMAGE > /dev/null
-NGB_DOCKER_BUILT=$?
-
-if [ $NGB_DOCKER_BUILT != 0 ]; then
-  echo "$NGB_DOCKER_IMAGE is not found, cannot build singularity image from it, exiting..."
-  exit 1
+if [ ! -d $SINGULARITY_WD ]; then
+  mkdir -p $SINGULARITY_WD
+  echo "Working dir $SINGULARITY_WD does not exist. Created one"
 fi
 
-# Setup local docker registry as working with docker locals is not supported by singularity
-# And push prebuilt NGB docker image to it
-docker run -d -p 5000:5000 --restart=always --name registry registry:2
-docker tag $NGB_DOCKER_IMAGE localhost:5000/$NGB_DOCKER_IMAGE
-docker push localhost:5000/$NGB_DOCKER_IMAGE
-
-export SINGULARITY_NOHTTPS=1
+cd $SINGULARITY_WD
+echo "Current working dir is $SINGULARITY_WD"
 
 singularity build "$SINGULARITY_IMAGE" "$SINGULARITY_FILE"
+
+cd -
--- singularity/setup_environment.sh
+++ singularity/setup_environment.sh
@@ -1,37 +1,56 @@
 set -e
 
-# 1. Install singlularity
+SINGULARITY_VERSION=${1:-master}
 
-## 1.1 Install deps
-sudo apt-get update && \
-sudo apt-get install -y build-essential \
-                        libssl-dev \
-                        uuid-dev \
-                        libgpgme11-dev \
-                        squashfs-tools \
-                        libseccomp-dev \
-                        pkg-config
 
-## 1.2 Install GO
-export VERSION=1.11 OS=linux ARCH=amd64
-cd /tmp
-wget -q https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz
-sudo tar -C /usr/local -xzf go$VERSION.$OS-$ARCH.tar.gz
-echo 'export GOPATH=${HOME}/go' >> ~/.bashrc
-echo 'export PATH=/usr/local/go/bin:${PATH}:${GOPATH}/bin' >> ~/.bashrc
-source ~/.bashrc
-mkdir -p ${GOPATH}/bin
-curl https://raw.githubusercontent.com/golang/dep/master/install.sh | sh
+if [[ "$SINGULARITY_VERSION" == "3."* ]] || [[ "$SINGULARITY_VERSION" == "master" ]]; then
+  # 1. Install singlularity
+  ## 1.1 Install deps
+  sudo apt-get update -q && \
+  sudo apt-get install -q -y build-essential \
+                          libssl-dev \
+                          uuid-dev \
+                          libgpgme11-dev \
+                          squashfs-tools \
+                          libseccomp-dev \
+                          pkg-config
 
-## 1.3 Clone singularity
-mkdir -p $GOPATH/src/github.com/sylabs
-cd $GOPATH/src/github.com/sylabs
-git clone https://github.com/sylabs/singularity.git
-cd singularity
+  ## 1.2 Install GO
+  export VERSION=1.11 OS=linux ARCH=amd64
+  cd /tmp
+  wget -q https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz
+  sudo tar -C /usr/local -xzf go$VERSION.$OS-$ARCH.tar.gz
+  echo 'export GOPATH=${HOME}/go' >> ~/.bashrc
+  echo 'export PATH=/usr/local/go/bin:${PATH}:${GOPATH}/bin' >> ~/.bashrc
+  source ~/.bashrc
+  mkdir -p ${GOPATH}/bin
+  curl https://raw.githubusercontent.com/golang/dep/master/install.sh | sh
 
-## 1.4 Build singularity
-cd $GOPATH/src/github.com/sylabs/singularity
-./mconfig
-cd ./builddir
-make
-sudo make install
+  ## 1.3 Clone singularity
+  mkdir -p $GOPATH/src/github.com/sylabs
+  cd $GOPATH/src/github.com/sylabs
+  git clone https://github.com/sylabs/singularity.git
+  cd singularity
+  git checkout $SINGULARITY_VERSION
+
+  ## 1.4 Build singularity
+  cd $GOPATH/src/github.com/sylabs/singularity
+  ./mconfig
+  cd ./builddir
+  make
+  sudo make install
+elif [[ "$SINGULARITY_VERSION" == "2."* ]]; then
+  sudo apt-get update && \
+  sudo apt-get install -y -q build-essential \
+                          python
+
+  wget "https://github.com/singularityware/singularity/releases/download/${SINGULARITY_VERSION}/singularity-${SINGULARITY_VERSION}.tar.gz"
+  tar -xvzf singularity-${SINGULARITY_VERSION}.tar.gz
+  cd singularity-${SINGULARITY_VERSION}
+  ./configure --prefix=/usr/local
+  make
+  sudo make install
+else
+  echo "Singularity $SINGULARITY_VERSION is not supported, exiting..."
+  exit 1
+fi
