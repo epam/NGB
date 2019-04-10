5152d614a973c3dbb24e5b519449e189111ebd7d
pavel_silin <pavel_silin@epam.com>
2019-04-10 15:04:46 +0300

    pmd fix

--- server/catgenome/src/main/java/com/epam/catgenome/manager/FileManager.java
+++ server/catgenome/src/main/java/com/epam/catgenome/manager/FileManager.java
@@ -49,7 +49,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Iterator;
