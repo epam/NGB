6c2e6d73b6e976f7f9548ccc472c8176b6b28549
Mike <mike.miroliubov@gmail.com>
2020-12-05 12:33:28 -0500

    Debug AppVeyor script

--- publish.sh
+++ publish.sh
@@ -31,7 +31,10 @@ if [ $? -ne 0 ]; then
     exit 0
 fi
 
+echo "branch: $APPVEYOR_REPO_BRANCH"
+
 if [[ "$APPVEYOR_REPO_BRANCH" == "develop" ]] || [[ "$APPVEYOR_REPO_BRANCH" == "release/"* ]]; then
+  echo "Publishing into Docker repository: $DOCKER_USER"
   docker login -u $DOCKER_USER -p $DOCKER_PSWD
   docker tag ngb:latest $DOCKER_USER/ngb:$NGB_VERSION
   docker push $DOCKER_USER/ngb:$NGB_VERSION
