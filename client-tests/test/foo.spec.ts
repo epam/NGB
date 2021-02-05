2b70bee7b298f2a733ea097b3d3a7e4969f40041
Aleksandr Kanunnikov <lifeart92@gmail.com>
2021-02-05 15:19:24 +0300

    code cleanup

--- client-tests/test/foo.spec.ts
+++ client-tests/test/foo.spec.ts
@@ -4,13 +4,108 @@ async function setupPage(page) {
   page.setViewportSize({width: 1500, height: 800});
 }
 
+interface ITrack {
+  b: string;
+  h?: number;
+  p: string;
+  n?: string;
+  l?: boolean;
+  projectIdNumber?: number;
+  f?: 'WIG' | 'BAM' | 'VCF' | 'GENE' | 'REFERENCE',
+  s?: {
+    rsfs?: boolean;
+    rsrs?: boolean;
+    rt?: false;
+    a?: boolean;
+    d?: boolean;
+    aa?: boolean;
+    m?: boolean;
+    c?: 'noColor' | 'insertSize';
+    i?: boolean;
+    g?: 'collapsed' | 'expanded';
+    s?: boolean;
+    v?: 'Collapsed';
+    c1?: boolean;
+    g1?: 'default';
+    r?: string | number; // ERROR!
+    s1?: boolean;
+    s2?: boolean;
+    s3?: boolean;
+    v1?: boolean;
+    cdm?: 'Bar Graph' | 'Heat Map';
+    cls?: boolean;
+    csm?: 'default';
+    csf?: number;
+    cst?: number;
+    he?: {
+      fontSize: string;
+    },
+    wigColors?: {
+      negative: number;
+      positive: number;
+    }
+  }
+}
+
+const hostname = `http://localhost:8080`;
+
+
+const pageObject = {
+  buttons: {
+    settings: 'body > ui-view > div > div.left-menu > ngb-main-toolbar > div > ul > li:nth-child(5) > ngb-main-settings > button'
+  },
+  windows: {
+    settings:  {
+      container: 'body > div.md-dialog-container.ng-scope > md-dialog > form > md-toolbar > div > h2',
+      tabs: {
+        alignments: {
+          container: 'md-tabs > md-tabs-wrapper > md-tabs-canvas > md-pagination-wrapper > md-tab-item:nth-child(2) > span',
+          content: '[role="tabpanel"]',
+          checkboxes: {
+            downsampling: '[aria-label="Downsample reads"]'
+          },
+          buttons: {
+            save: 'body > div.md-dialog-container.ng-scope > md-dialog > form > md-dialog-actions > button.md-primary.md-button.md-ink-ripple > span'
+          }
+        }
+      },
+    },
+    main: {
+      trackSettings: {
+        groupBy: {
+          container: 'body > ui-view > div > div.golden-layout > ngb-golden-layout > div > div > div > div:nth-child(1) > div.lm_items > div > div > ngb-browser > ngb-tracks-view > div > div > div:nth-child(2) > ngb-track:nth-child(3) > div > div.ngb-track-header.layout-align-start-center.layout-row.flex-100 > ngb-track-settings > md-menu:nth-child(5) > button > a:nth-child(1)',
+          options: {
+            startLocation: '#menu_container_11 > md-menu-content > div:nth-child(2) > md-menu-item > button > span:nth-child(1)',
+            insertSize: '#menu_container_11 > md-menu-content > div:nth-child(6) > md-menu-item > button > span:nth-child(1)',
+          }
+        },
+      },
+      canvas: 'body > ui-view > div > div.golden-layout > ngb-golden-layout > div > div > div > div:nth-child(1) > div.lm_items > div > div > ngb-browser > ngb-tracks-view > div > div > div:nth-child(2) > ngb-track:nth-child(3) > div > div.ngb-track-renderer.js-ngb-render-container-target.flex-100 > div > canvas'
+    }
+  }, 
+}
+
+function buildUrl(referenceId: string, chromosome: string = '', start: string, end: string, tracks: ITrack[] = []) {
+  return `${hostname}/#/${[referenceId, chromosome, start, end].filter(el => el.length).join('/')}?tracks=${encodeURIComponent(JSON.stringify(tracks))}`;
+}
+
 describe('foo', () => {
 
   xit("is a basic test with the page", async ({ page }) => {
 
     await setupPage(page);
 
-    const url = 'http://localhost:8080/#/DM6/?tracks=%5B%7B%22b%22:%22DM6%22,%22p%22:%22Fruitfly%22,%22f%22:%22REFERENCE%22,%22projectIdNumber%22:1%7D,%7B%22b%22:%22agnts3.09-28.trim.dm606.realign.bam%22,%22p%22:%22Fruitfly%22,%22f%22:%22BAM%22,%22projectIdNumber%22:1%7D,%7B%22b%22:%22agnX1.09-28.trim.dm606.realign.bam%22,%22p%22:%22Fruitfly%22,%22f%22:%22BAM%22,%22projectIdNumber%22:1%7D,%7B%22b%22:%22agnX1.09-28.trim.dm606.realign.vcf%22,%22p%22:%22Fruitfly%22,%22f%22:%22VCF%22,%22projectIdNumber%22:1%7D,%7B%22b%22:%22CantonS.09-28.trim.dm606.realign.bam%22,%22p%22:%22Fruitfly%22,%22f%22:%22BAM%22,%22projectIdNumber%22:1%7D,%7B%22b%22:%22CantonS.09-28.trim.dm606.realign.vcf%22,%22p%22:%22Fruitfly%22,%22f%22:%22VCF%22,%22projectIdNumber%22:1%7D,%7B%22b%22:%22DM6_Genes%22,%22p%22:%22%22,%22l%22:true,%22f%22:%22GENE%22%7D%5D';
+    const tracks: ITrack[] = [
+      {"b":"DM6","p":"Fruitfly","f":"REFERENCE","projectIdNumber":1},
+      {"b":"agnts3.09-28.trim.dm606.realign.bam","p":"Fruitfly","f":"BAM","projectIdNumber":1},
+      {"b":"agnX1.09-28.trim.dm606.realign.bam","p":"Fruitfly","f":"BAM","projectIdNumber":1},
+      {"b":"agnX1.09-28.trim.dm606.realign.vcf","p":"Fruitfly","f":"VCF","projectIdNumber":1},
+      {"b":"CantonS.09-28.trim.dm606.realign.bam","p":"Fruitfly","f":"BAM","projectIdNumber":1},
+      {"b":"CantonS.09-28.trim.dm606.realign.vcf","p":"Fruitfly","f":"VCF","projectIdNumber":1},
+      {"b":"DM6_Genes","p":"","l":true,"f":"GENE"}];
+
+    const url = buildUrl('DM6', '', '', '', tracks);
+
     await page.goto(url);
 
     await page.waitForSelector('.ngb-variant-density-diagram .nvd3-svg g');
@@ -37,8 +132,15 @@ describe('foo', () => {
 
     await setupPage(page);
 
-    const url = 'http://localhost:8080/#/DM6/X/1/23542271?tracks=%5B%7B%22b%22:%22agnts3.09-28.trim.dm606.realign.bam%22,%22f%22:%22BAM%22,%22h%22:250,%22p%22:%22Fruitfly%22,%22s%22:%7B%22a%22:true,%22aa%22:true,%22c%22:%22noColor%22,%22c1%22:true,%22d%22:true,%22g1%22:%22default%22,%22i%22:true,%22m%22:true,%22r%22:%221%22,%22s1%22:false,%22s2%22:true,%22s3%22:false,%22v1%22:false,%22cdm%22:%22Bar%20Graph%22,%22cls%22:false,%22csm%22:%22default%22%7D,%22projectIdNumber%22:1%7D,%7B%22b%22:%22agnX1.09-28.trim.dm606.realign.bam%22,%22f%22:%22BAM%22,%22h%22:250,%22p%22:%22Fruitfly%22,%22s%22:%7B%22a%22:true,%22aa%22:true,%22c%22:%22noColor%22,%22c1%22:true,%22d%22:true,%22g1%22:%22default%22,%22i%22:true,%22m%22:true,%22r%22:%221%22,%22s1%22:false,%22s2%22:true,%22s3%22:false,%22v1%22:false,%22cdm%22:%22Bar%20Graph%22,%22cls%22:false,%22csm%22:%22default%22%7D,%22projectIdNumber%22:1%7D,%7B%22b%22:%22DM6%22,%22f%22:%22REFERENCE%22,%22h%22:100,%22p%22:%22Fruitfly%22,%22s%22:%7B%22rt%22:false,%22rsfs%22:true,%22rsrs%22:false%7D,%22projectIdNumber%22:1%7D,%7B%22b%22:%22agnX1.09-28.trim.dm606.realign.vcf%22,%22f%22:%22VCF%22,%22h%22:70,%22p%22:%22Fruitfly%22,%22s%22:%7B%22v%22:%22Collapsed%22%7D,%22projectIdNumber%22:1%7D,%7B%22b%22:%22CantonS.09-28.trim.dm606.realign.bam%22,%22f%22:%22BAM%22,%22h%22:250,%22p%22:%22Fruitfly%22,%22s%22:%7B%22a%22:true,%22aa%22:true,%22c%22:%22noColor%22,%22c1%22:true,%22d%22:true,%22g1%22:%22default%22,%22i%22:true,%22m%22:true,%22r%22:%221%22,%22s1%22:false,%22s2%22:true,%22s3%22:false,%22v1%22:false,%22cdm%22:%22Bar%20Graph%22,%22cls%22:false,%22csm%22:%22default%22%7D,%22projectIdNumber%22:1%7D,%7B%22b%22:%22CantonS.09-28.trim.dm606.realign.vcf%22,%22f%22:%22VCF%22,%22h%22:70,%22p%22:%22Fruitfly%22,%22s%22:%7B%22v%22:%22Collapsed%22%7D,%22projectIdNumber%22:1%7D,%7B%22b%22:%22DM6_Genes%22,%22f%22:%22GENE%22,%22h%22:158,%22l%22:true,%22p%22:%22%22,%22s%22:%7B%22g%22:%22collapsed%22%7D%7D%5D';
-      
+    const tracks: ITrack[] = [
+        {"b":"agnts3.09-28.trim.dm606.realign.bam","f":"BAM","h":250,"p":"Fruitfly","s":{"a":true,"aa":true,"c":"noColor","c1":true,"d":true,"g1":"default","i":true,"m":true,"r":"1","s1":false,"s2":true,"s3":false,"v1":false,"cdm":"Bar Graph","cls":false,"csm":"default"},"projectIdNumber":1},
+        {"b":"agnX1.09-28.trim.dm606.realign.bam","f":"BAM","h":250,"p":"Fruitfly","s":{"a":true,"aa":true,"c":"noColor","c1":true,"d":true,"g1":"default","i":true,"m":true,"r":"1","s1":false,"s2":true,"s3":false,"v1":false,"cdm":"Bar Graph","cls":false,"csm":"default"},"projectIdNumber":1},
+        {"b":"DM6","f":"REFERENCE","h":100,"p":"Fruitfly","s":{"rt":false,"rsfs":true,"rsrs":false},"projectIdNumber":1},{"b":"agnX1.09-28.trim.dm606.realign.vcf","f":"VCF","h":70,"p":"Fruitfly","s":{"v":"Collapsed"},"projectIdNumber":1},
+        {"b":"CantonS.09-28.trim.dm606.realign.bam","f":"BAM","h":250,"p":"Fruitfly","s":{"a":true,"aa":true,"c":"noColor","c1":true,"d":true,"g1":"default","i":true,"m":true,"r":"1","s1":false,"s2":true,"s3":false,"v1":false,"cdm":"Bar Graph","cls":false,"csm":"default"},"projectIdNumber":1},
+        {"b":"CantonS.09-28.trim.dm606.realign.vcf","f":"VCF","h":70,"p":"Fruitfly","s":{"v":"Collapsed"},"projectIdNumber":1},{"b":"DM6_Genes","f":"GENE","h":158,"l":true,"p":"","s":{"g":"collapsed"}}];
+
+    const url = buildUrl('DM6', 'X', '1', '23542271', tracks);
+
     await page.goto(url);
 
     // animations
@@ -64,7 +166,11 @@ describe('foo', () => {
 
     await setupPage(page);
 
-    const url = 'http://localhost:8080/#/h37/1/150966170/150966249?tracks=%5B%7B%22b%22:%22h37%22,%22p%22:%22h37_data%22,%22f%22:%22REFERENCE%22,%22projectIdNumber%22:1,%22h%22:100,%22s%22:%7B%22rt%22:false,%22rsfs%22:true,%22rsrs%22:false%7D%7D,%7B%22b%22:%22sv_sample_2.bw%22,%22h%22:86,%22p%22:%22h37_data%22,%22projectIdNumber%22:1,%22s%22:%7B%22cdm%22:%22Bar%20Graph%22,%22cls%22:false,%22csm%22:%22default%22,%22csf%22:100,%22cst%22:900,%22wigColors%22:%7B%22negative%22:16731471,%22positive%22:3265094%7D%7D,%22n%22:%22sv_sample_2.bw%22,%22f%22:%22WIG%22%7D%5D';
+    const tracks: ITrack[] = [
+      {"b":"h37","p":"h37_data","f":"REFERENCE","projectIdNumber":1,"h":100,"s":{"rt":false,"rsfs":true,"rsrs":false}},
+      {"b":"sv_sample_2.bw","h":86,"p":"h37_data","projectIdNumber":1,"s":{"cdm":"Bar Graph","cls":false,"csm":"default","csf":100,"cst":900,"wigColors":{"negative":16731471,"positive":3265094}},"n":"sv_sample_2.bw","f":"WIG"}];
+    
+    const url = buildUrl('h37', '1', '150966170', '150966249', tracks);
     await page.goto(url);
 
     // animations
@@ -79,7 +185,12 @@ describe('foo', () => {
 
     await setupPage(page);
 
-    const url = 'http://localhost:8080/#/h37/1/150964883/150967542?tracks=%5B%7B%22b%22:%22h37%22,%22p%22:%22h37_data%22,%22f%22:%22REFERENCE%22,%22projectIdNumber%22:1,%22h%22:100,%22s%22:%7B%22rt%22:false,%22rsfs%22:true,%22rsrs%22:false%7D%7D,%7B%22b%22:%22Homo_sapiens.GRCh37.gtf.gz%22,%22h%22:84,%22l%22:true,%22p%22:%22%22,%22s%22:%7B%22g%22:%22expanded%22%7D,%22f%22:%22GENE%22%7D,%7B%22b%22:%22sv_sample_2.bw%22,%22h%22:150,%22p%22:%22h37_data%22,%22projectIdNumber%22:1,%22s%22:%7B%22cdm%22:%22Bar%20Graph%22,%22cls%22:false,%22csm%22:%22default%22,%22csf%22:100,%22cst%22:900,%22wigColors%22:%7B%22negative%22:16731471,%22positive%22:3265094%7D%7D,%22n%22:%22sv_sample_2.bw%22,%22f%22:%22WIG%22%7D%5D';
+    const tracks: ITrack[] = [
+      {"b":"h37","p":"h37_data","f":"REFERENCE","projectIdNumber":1,"h":100,"s":{"rt":false,"rsfs":true,"rsrs":false}},{"b":"Homo_sapiens.GRCh37.gtf.gz","h":84,"l":true,"p":"","s":{"g":"expanded"},"f":"GENE"},
+      {"b":"sv_sample_2.bw","h":150,"p":"h37_data","projectIdNumber":1,"s":{"cdm":"Bar Graph","cls":false,"csm":"default","csf":100,"cst":900,"wigColors":{"negative":16731471,"positive":3265094}},"n":"sv_sample_2.bw","f":"WIG"}];
+    
+    const url = buildUrl('h37', '1', '150964883', '150967542', tracks);
+
     await page.goto(url);
 
     // animations
@@ -91,8 +202,13 @@ describe('foo', () => {
 
   it('works for track renderer', async ({page}) => {
     
-    page.setViewportSize({width: 1500, height: 800});
-    const url = 'http://localhost:8080/#/dm6/X/12584385/12592193?tracks=%5B%7B%22b%22:%22dm6%22,%22p%22:%22dm6_data%22,%22f%22:%22REFERENCE%22,%22projectIdNumber%22:2,%22h%22:100,%22s%22:%7B%22rt%22:false,%22rsfs%22:true,%22rsrs%22:false%7D%7D,%7B%22b%22:%22agnX1.09-28.trim.dm606.realign.bam%22,%22h%22:576,%22p%22:%22dm6_data%22,%22projectIdNumber%22:2,%22s%22:%7B%22a%22:true,%22aa%22:true,%22c%22:%22insertSize%22,%22c1%22:true,%22d%22:true,%22g1%22:%22default%22,%22he%22:%7B%22fontSize%22:%2216px%22%7D,%22i%22:true,%22m%22:true,%22r%22:2,%22s1%22:true,%22s2%22:true,%22s3%22:false,%22v1%22:false,%22cdm%22:%22Bar%20Graph%22,%22cls%22:false,%22csm%22:%22default%22%7D,%22n%22:%22agnX1.09-28.trim.dm606.realign.bam%22,%22f%22:%22BAM%22%7D%5D';
+    await setupPage(page);
+
+    const tracks: ITrack[] = [
+      {"b":"dm6","p":"dm6_data","f":"REFERENCE","projectIdNumber":2,"h":100,"s":{"rt":false,"rsfs":true,"rsrs":false}},
+      {"b":"agnX1.09-28.trim.dm606.realign.bam","h":576,"p":"dm6_data","projectIdNumber":2,"s":{"a":true,"aa":true,"c":"insertSize","c1":true,"d":true,"g1":"default","he":{"fontSize":"16px"},"i":true,"m":true,"r":2,"s1":true,"s2":true,"s3":false,"v1":false,"cdm":"Bar Graph","cls":false,"csm":"default"},"n":"agnX1.09-28.trim.dm606.realign.bam","f":"BAM"}];
+    
+    const url = buildUrl('dm6', 'X', '12584385', '12592193', tracks);
   
     await page.goto(url);
 
@@ -100,13 +216,13 @@ describe('foo', () => {
     await page.waitForTimeout(10000);
 
     // group-by
-    await page.click('body > ui-view > div > div.golden-layout > ngb-golden-layout > div > div > div > div:nth-child(1) > div.lm_items > div > div > ngb-browser > ngb-tracks-view > div > div > div:nth-child(2) > ngb-track:nth-child(3) > div > div.ngb-track-header.layout-align-start-center.layout-row.flex-100 > ngb-track-settings > md-menu:nth-child(5) > button > a:nth-child(1)');
+    await page.click(pageObject.windows.main.trackSettings.groupBy.container);
     
     // by start location
-    await page.click('#menu_container_11 > md-menu-content > div:nth-child(2) > md-menu-item > button > span:nth-child(1)');
+    await page.click(pageObject.windows.main.trackSettings.groupBy.options.startLocation);
 
     // by insert size
-    // await page.click('#menu_container_11 > md-menu-content > div:nth-child(6) > md-menu-item > button > span:nth-child(1)');
+    // await page.click();
 
     await page.evaluate(() => {
       var $0 = document.querySelector('body > ui-view > div > div.golden-layout > ngb-golden-layout > div > div > div > div:nth-child(1) > div.lm_items > div > div > ngb-browser > ngb-tracks-view > div > div > div:nth-child(2) > ngb-track:nth-child(3) > div > div.ngb-track-renderer.js-ngb-render-container-target.flex-100 > div > canvas');
@@ -128,7 +244,7 @@ describe('foo', () => {
       context.putImageData(imageData, 0, 0);
     });
 
-    const element = await page.$(`body > ui-view > div > div.golden-layout > ngb-golden-layout > div > div > div > div:nth-child(1) > div.lm_items > div > div > ngb-browser > ngb-tracks-view > div > div > div:nth-child(2) > ngb-track:nth-child(3) > div > div.ngb-track-renderer.js-ngb-render-container-target.flex-100 > div > canvas`);
+    const element = await page.$(pageObject.windows.main.canvas);
 
     // const coords = await element.boundingBox();
     const screenshot = await element.screenshot({ omitBackground: true });
@@ -138,39 +254,40 @@ describe('foo', () => {
   
 
   it('matches with disabled downsampling', async ({page}) => {
-    page.setViewportSize({width: 1500, height: 800});
-    const url = 'http://localhost:8080/#/dm6/X/12584385/12592193?tracks=%5B%7B%22b%22:%22dm6%22,%22p%22:%22dm6_data%22,%22f%22:%22REFERENCE%22,%22projectIdNumber%22:2,%22h%22:100,%22s%22:%7B%22rt%22:false,%22rsfs%22:true,%22rsrs%22:false%7D%7D,%7B%22b%22:%22agnX1.09-28.trim.dm606.realign.bam%22,%22h%22:576,%22p%22:%22dm6_data%22,%22projectIdNumber%22:2,%22s%22:%7B%22a%22:true,%22aa%22:true,%22c%22:%22insertSize%22,%22c1%22:true,%22d%22:true,%22g1%22:%22default%22,%22he%22:%7B%22fontSize%22:%2216px%22%7D,%22i%22:true,%22m%22:true,%22r%22:2,%22s1%22:true,%22s2%22:true,%22s3%22:false,%22v1%22:false,%22cdm%22:%22Bar%20Graph%22,%22cls%22:false,%22csm%22:%22default%22%7D,%22n%22:%22agnX1.09-28.trim.dm606.realign.bam%22,%22f%22:%22BAM%22%7D%5D';
+
+    await setupPage(page);
+
+    const tracks: ITrack[] = [{"b":"dm6","p":"dm6_data","f":"REFERENCE","projectIdNumber":2,"h":100,"s":{"rt":false,"rsfs":true,"rsrs":false}},{"b":"agnX1.09-28.trim.dm606.realign.bam","h":576,"p":"dm6_data","projectIdNumber":2,"s":{"a":true,"aa":true,"c":"insertSize","c1":true,"d":true,"g1":"default","he":{"fontSize":"16px"},"i":true,"m":true,"r":2,"s1":true,"s2":true,"s3":false,"v1":false,"cdm":"Bar Graph","cls":false,"csm":"default"},"n":"agnX1.09-28.trim.dm606.realign.bam","f":"BAM"}];
+    
+    const url = buildUrl('dm6', 'X', '12584385', '12592193', tracks);
   
     await page.goto(url);
 
     // animations
     await page.waitForTimeout(10000);
-    
+
     // settings button
-    await page.click('body > ui-view > div > div.left-menu > ngb-main-toolbar > div > ul > li:nth-child(5) > ngb-main-settings > button');
+    await page.click(pageObject.buttons.settings);
   
     // settings window
-    await page.waitForSelector('body > div.md-dialog-container.ng-scope > md-dialog > form > md-toolbar > div > h2');
+    await page.waitForSelector(pageObject.windows.settings.container);
  
-
     // aligments tab in settings
-
-    await page.click('md-tabs > md-tabs-wrapper > md-tabs-canvas > md-pagination-wrapper > md-tab-item:nth-child(2) > span');
+    await page.click(pageObject.windows.settings.tabs.alignments.container);
     
-
     // wait for tab
-    await page.waitForSelector('[role="tabpanel"]');
+    await page.waitForSelector(pageObject.windows.settings.tabs.alignments.content);
 
     // downsampling uncheck
-    await page.click('[aria-label="Downsample reads"]');
+    await page.click(pageObject.windows.settings.tabs.alignments.checkboxes.downsampling);
   
     // save button
-    await page.click('body > div.md-dialog-container.ng-scope > md-dialog > form > md-dialog-actions > button.md-primary.md-button.md-ink-ripple > span');
+    await page.click(pageObject.windows.settings.tabs.alignments.buttons.save);
   
     // (data loading & redraw)
     await page.waitForTimeout(10000);
 
-    const element = await page.$(`body > ui-view > div > div.golden-layout > ngb-golden-layout > div > div > div > div:nth-child(1) > div.lm_items > div > div > ngb-browser > ngb-tracks-view > div > div > div:nth-child(2) > ngb-track:nth-child(3) > div > div.ngb-track-renderer.js-ngb-render-container-target.flex-100 > div > canvas`);
+    const element = await page.$(pageObject.windows.main.canvas);
 
     // const coords = await element.boundingBox();
     const screenshot = await element.screenshot({ omitBackground: true });
