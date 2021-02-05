1375f16edb3e7ec2cd30b4bd344d3d042daf238f
Aleksandr Kanunnikov <lifeart92@gmail.com>
2021-02-05 16:30:39 +0300

    url converter

---
+++ client-tests/utils/url-converter/index.js
@@ -0,0 +1,40 @@
+
+import fs from 'fs';
+
+const input = fs.readFileSync('input.txt', 'utf8');
+
+const urls = input.split(/\r?\n/).map(url => {
+    let raw = url.trim();
+    let correctStart = raw.slice(raw.indexOf('http'), raw.length);
+    return correctStart.split(' ')[0];
+});
+
+const results = [];
+
+urls.forEach((url) => {
+    let urlObject = new URL(url);
+    let [ head, tail ] = urlObject.hash.split('?');
+    let headKeys = ['referenceId', 'chromosome', 'start', 'end'];
+    let result = { };
+
+    head.split('/').forEach((el) => {
+        if (el.length && el !== '#') {
+            let key = headKeys.shift();
+            result[key] = el;
+        }
+    });
+
+    tail.split('&').forEach((queryPair) => {
+        const [ queryParamName, queryParamValue ] = queryPair.split('=');
+        result[queryParamName] = queryParamValue;
+    });
+
+    if (result['tracks']) {
+        result['tracks'] = JSON.parse(decodeURIComponent(result['tracks']));
+    }
+
+    results.push(result);
+});
+
+
+fs.writeFileSync('output.json', JSON.stringify(results), 'utf8');
