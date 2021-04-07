f5b097fc8b7449f0260ec06f521abd2793b17f50
Zhanna Kaymedenova <zh.kaymedenova@gmail.com>
2021-04-07 17:12:08 +0300

    feat: added initBuildColumns function

--- client/client/app/components/ngbBlastSearchPanel/ngbBlastSearch.service.js
+++ client/client/app/components/ngbBlastSearchPanel/ngbBlastSearch.service.js
@@ -1,25 +1,24 @@
 export default class ngbBlastSearchService {
-  static instance(projectContext){
-    return new ngbBlastSearchService(projectContext);
-  }
-  constructor(projectContext){
-    Object.assign(this, {
-      projectContext
-    })
-  }
+    static instance(projectContext){
+        return new ngbBlastSearchService(projectContext);
+    }
+    constructor(projectContext){
+        Object.assign(this, { projectContext });
+    }
 
-  generateBlastSearchResults(){
-    let results = [];
-    this.projectContext.chromosomes.slice(0,8).forEach(chr => {
-      const pos1 = Math.floor(Math.random() * chr.size);
-      const pos2 = Math.floor(Math.random() * chr.size);
-      const pos3 = Math.floor(Math.random() * chr.size);
-      const pos4 = Math.floor(Math.random() * chr.size);
-      results.push({ startIndex: Math.min(pos1, pos2), endIndex:Math.max(pos1, pos2), chromosome:chr.name });
-      results.push({ startIndex: Math.min(pos1, pos2), endIndex:Math.max(pos1, pos2), chromosome:chr.name });
-      results.push({ startIndex: Math.min(pos3, pos4), endIndex:Math.max(pos3, pos4), chromosome:chr.name });
-      results.push({ startIndex: Math.min(pos3, pos4), endIndex:Math.max(pos3, pos4), chromosome:chr.name });
-    })
-    return results;
+    generateBlastSearchResults() {
+        const results = [];
+        this.projectContext.chromosomes.slice(0, 8).forEach(chr => {
+            for (let i = 0; i < 1000; i++) {
+                const start = 1 + Math.floor(Math.random() * (chr.size - 1));
+                const singleSized = Math.random() >= 0.5;
+                const end = Math.min(
+                    start + (singleSized ? 1 : Math.floor(Math.random() * chr.size / 100)),
+                    chr.size,
+                );
+                results.push({startIndex: start, endIndex: end, chromosome: chr.name});
+            }
+        });
+        return results;
   }
 }
--- client/client/modules/render/whole-genome-render/partials/chromosomeColumn/chromosomeColumnRenderer.js
+++ client/client/modules/render/whole-genome-render/partials/chromosomeColumn/chromosomeColumnRenderer.js
@@ -46,87 +46,120 @@ export class ChromosomeColumnRenderer {
         return container;
     }
 
-    createColumn(container, position, chrPixelValue, chromosome, hits) {
+    createColumn(container, position, chrPixelValue, chromosome, hitsColumns) {
 
-      const column = new PIXI.Graphics();
-      column.x = position.x;
-      column.y = 0;
-      container.addChild(column);
+        const column = new PIXI.Graphics();
+        column.x = position.x;
+        column.y = 0;
+        container.addChild(column);
 
-      column
-          .beginFill(config.chromosomeColumn.fill, 1)
-          .drawRect(
-              position.x,
-              0,
-              this.columnWidth,
-              chrPixelValue)
-          .endFill()
-          .lineStyle(config.chromosomeColumn.thickness, config.chromosomeColumn.lineColor, 1)
-          .moveTo(position.x, 0)
-          .lineTo(position.x + this.columnWidth, 0)
-          .lineTo(position.x + this.columnWidth, chrPixelValue)
-          .lineTo(position.x, chrPixelValue)
-          .lineTo(position.x, 0)
-          .moveTo(position.x + config.hit.offset, 0)
-      
-      const sortedHits = this.sortHitsByLength(hits);
-      for (let i = 0; i < sortedHits.length && i <= this.hitsLimit; i++){
+        column
+            .beginFill(config.chromosomeColumn.fill, 1)
+            .drawRect(
+                position.x,
+                0,
+                this.columnWidth,
+                chrPixelValue)
+            .endFill()
+            .lineStyle(config.chromosomeColumn.thickness, config.chromosomeColumn.lineColor, 1)
+            .moveTo(position.x, 0)
+            .lineTo(position.x + this.columnWidth, 0)
+            .lineTo(position.x + this.columnWidth, chrPixelValue)
+            .lineTo(position.x, chrPixelValue)
+            .lineTo(position.x, 0)
+            .moveTo(position.x + config.hit.offset, 0)
 
-          let hit = sortedHits[i];
-          let offset = config.hit.offset;
+            let offset = config.hit.offset;
 
-          const hitPixelLength = this.convertToPixels(hit.endIndex - hit.startIndex);
-          const hitPixelStart =  (hit.startIndex/chromosome.size) * chrPixelValue;
+            for (let colIndex = 0; colIndex < hitsColumns.length && colIndex <= this.hitsLimit; colIndex++){
 
-          column 
-            .lineStyle(config.hit.width, config.hit.lineColor, 1)
-            .moveTo(position.x + this.columnWidth +  offset * (i+1), hitPixelStart)
-            .lineTo(position.x + this.columnWidth + offset * (i+1), hitPixelStart + hitPixelLength);
-      }
+            const hitColumn = hitsColumns[colIndex];
 
-      const label = this.createLabel(`chr ${chromosome.id}`, position);
-      container.addChild(label);
+                for (let hitIndex = 0; hitIndex < hitColumn.length; hitIndex++){
+                    let hit = hitColumn[hitIndex];
+                    const hitPixelLength = this.convertToPixels(hit.endIndex - hit.startIndex);
+                    const hitPixelStart =  (hit.startIndex/chromosome.size) * chrPixelValue;
+                    column 
+                        .lineStyle(config.hit.width, config.hit.lineColor, 1)
+                        .moveTo(position.x + this.columnWidth + offset, hitPixelStart)
+                        .lineTo(position.x + this.columnWidth + offset, hitPixelStart + hitPixelLength);
+                }
+
+                offset += config.hit.offset;
+            }
+
+            const label = this.createLabel(`chr ${chromosome.id}`, position);
+            container.addChild(label);
     }
 
     buildColumns(container) {
 
-      let position = {
-          x: config.start.x,
-          y: 0 
-      }
-      
-      for (let i = 0; i < this.chromosomes.length; i++){
-
-        let chr = this.chromosomes[i];
-        const chrHits = this.hits.filter(hit => hit.chromosome === chr.name);
-
-        const pixelSize = this.convertToPixels(chr.size);
-        this.createColumn(container, position, pixelSize, chr, chrHits);
-
-        position = {
-          x: position.x + config.start.margin,
-          y: 0
+        let position = {
+            x: config.start.x,
+            y: 0 
         }
-      }
+      
+        for (let i = 0; i < this.chromosomes.length; i++){
+
+            let chr = this.chromosomes[i];
+            const chrHits = this.hits.filter(hit => hit.chromosome === chr.name);
+
+            const pixelSize = this.convertToPixels(chr.size);
+            const sortedHits = this.sortHitsByLength(chrHits);
+            const hitsColumns = this.initBuildColumns(sortedHits);
+            this.createColumn(container, position, pixelSize, chr, hitsColumns);
+            position = {
+                x: position.x + config.start.margin,
+                y: 0
+            };
+        }
     }
 
     convertToPixels(size) {
-      return (size / this.maxChrSize) * this.containerHeight;
+        return (size / this.maxChrSize) * this.containerHeight;
     }
 
     createLabel(text, position) {
-      const label = new PIXI.Text(text, {
-        fill: config.tick.label.fill,
-        font: config.tick.label.font,
-        margin:config.tick.label.margin,
-    });
-      label.resolution = drawingConfiguration.resolution;
-      label.y = position.y - this.topMargin/2 - label.height/2;
-      label.x = 2 * position.x;
-      return label;
+
+        const label = new PIXI.Text(text, {
+            fill: config.tick.label.fill,
+            font: config.tick.label.font,
+            margin:config.tick.label.margin,
+        });
+
+        label.resolution = drawingConfiguration.resolution;
+        label.y = position.y - this.topMargin/2 - label.height/2;
+        label.x = 2 * position.x;
+        return label;
     }
 
     sortHitsByLength(hits){
         return hits.sort((hit1, hit2) => (hit2.endIndex - hit2.startIndex) - (hit1.endIndex - hit1.startIndex));
     }
+
+    initBuildColumns(hits, columnsArray = []) {
+
+        let remain = [];
+        let column = [];
+        const hitsColumns = columnsArray;
+
+        hits.forEach((el, index) => {
+            if (index === 0) {
+                column.push(el);
+            } else if (el.startIndex > column[column.length-1].endIndex){
+                column.push(el);
+            } else {
+                remain.push(el);
+            };
+        });
+
+        hitsColumns.push(column);
+
+        if (remain.length){
+                this.initBuildColumns(remain, hitsColumns);
+                return hitsColumns;
+        } else {
+                return hitsColumns;
+        };
+  }
 }
