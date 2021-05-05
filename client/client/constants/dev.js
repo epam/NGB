5416f57429262a930d781a338c5c72f38ca30545
Mikhail Rodichenko <Mikhail_Rodichenko@epam.com>
2021-05-05 11:59:43 +0300

    dev.js

--- client/client/constants/dev.js
+++ client/client/constants/dev.js
@@ -1,4 +1,4 @@
 module.exports = {
     env: 'development',
-    urlPrefix: 'http://oss-1204092014.us-east-1.elb.amazonaws.com:8080/catgenome'//'http://ngb.opensource.epam.com/catgenome/'
+    urlPrefix: 'http://ngb.opensource.epam.com/catgenome/'
 };
