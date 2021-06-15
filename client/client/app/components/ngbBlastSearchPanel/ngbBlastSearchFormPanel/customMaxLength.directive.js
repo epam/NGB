bca173281d117b1c3b64883f257e48aefc65a66f
Dmitrii Krasnov <dmitrii_krasnov@epam.com>
2021-06-15 10:34:07 +0300

    BLAST Search (#381): ngMessages fix. Revert blast form workaround

---
+++ client/client/app/components/ngbBlastSearchPanel/ngbBlastSearchFormPanel/customMaxLength.directive.js
@@ -0,0 +1,25 @@
+export default function () {
+    return {
+        restrict: 'A',
+        scope: {
+            customMaxLength: '<'
+        },
+        require: 'ngModel',
+        link(scope, elm, attrs, ngModel) {
+            // ngModel.$parsers.unshift(function(value) {
+            //     ngModel.$setValidity('customMaxLength', value.length <= scope.customMaxLength);
+            //     return value;
+            // });
+            // ngModel.$formatters.unshift(function(value) {
+            //     ngModel.$setValidity('customMaxLength', value.length <= scope.customMaxLength);
+            //     return value;
+            // });
+            ngModel.$validators.customMaxLength = function(modelValue) {
+                if (ngModel.$isEmpty(modelValue)) {
+                    return true;
+                }
+                return modelValue.length <= scope.customMaxLength;
+            };
+        }
+    };
+}
