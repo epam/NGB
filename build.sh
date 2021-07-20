3682c1950bdf714101ccf5c994a877cfe480b403
Aleksandr Sidoruk <aleksandr_sidoruk@epam.com>
2021-07-20 15:18:29 +0300

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [[ "$APPVEYOR_REPO_BRANCH" == "release/"* ]]; then
+  echo "Building with docker distribution"
+  BUILD_DOCKER=buildDocker
+fi
+
+./gradlew buildJar buildCli buildDoc $BUILD_DOCKER -PbuildNumber=${APPVEYOR_BUILD_NUMBER} -PnoTest
