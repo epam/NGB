ba5a8c56db5f70e67705126eeb1c1214f3435799
Dmitrii Krasnov <dmitrii_krasnov@epam.com>
2021-07-23 17:57:36 +0300

    Download data from "Genes" and "Variants" panels (#464): download error processing

--- client/client/app/components/ngbVariantsTablePanel/ngbVariantsTableDownload/ngbVariantsTableDownloadDlg.controller.js
+++ client/client/app/components/ngbVariantsTablePanel/ngbVariantsTableDownload/ngbVariantsTableDownloadDlg.controller.js
@@ -13,10 +13,12 @@ export default class ngbVariantsTableDownloadDlgController {
         }
     ];
     downloadFormat;
+    error;
 
-    constructor($mdDialog, projectContext) {
+    constructor($mdDialog, projectContext, $scope) {
         this.$mdDialog = $mdDialog;
         this.projectContext = projectContext;
+        this.$scope = $scope;
 
         this.downloadFormat = this.formatList[0];
     }
@@ -32,27 +34,33 @@ export default class ngbVariantsTableDownloadDlgController {
             this.downloadFormat.name,
             this.includeHeader
         ).then(data => {
-            const linkElement = document.createElement('a');
-            try {
-                const blob = new Blob([data], {type: this.downloadFormat.mimeType});
-                const url = window.URL.createObjectURL(blob);
+            if (data.error) {
+                this.error = 'Error during export happened';
+            } else {
+                this.error = '';
+                const linkElement = document.createElement('a');
+                try {
+                    const blob = new Blob([data], {type: this.downloadFormat.mimeType});
+                    const url = window.URL.createObjectURL(blob);
 
-                linkElement.setAttribute('href', url);
-                linkElement.setAttribute('download',
-                    `VCF-${this.projectContext.reference.id}.${this.downloadFormat.name.toLowerCase()}`);
+                    linkElement.setAttribute('href', url);
+                    linkElement.setAttribute('download',
+                        `VariantsTable"-${this.projectContext.reference.id}.${this.downloadFormat.name.toLowerCase()}`);
 
-                const clickEvent = new MouseEvent('click', {
-                    'view': window,
-                    'bubbles': true,
-                    'cancelable': false
-                });
-                linkElement.dispatchEvent(clickEvent);
-                this.close();
-            } catch (ex) {
-                // eslint-disable-next-line no-console
-                console.error(ex);
+                    const clickEvent = new MouseEvent('click', {
+                        'view': window,
+                        'bubbles': true,
+                        'cancelable': false
+                    });
+                    linkElement.dispatchEvent(clickEvent);
+                    this.close();
+                } catch (ex) {
+                    // eslint-disable-next-line no-console
+                    console.error(ex);
+                }
             }
             this.isLoading = false;
+            this.$scope.$apply();
         });
         event.stopImmediatePropagation();
         return false;
