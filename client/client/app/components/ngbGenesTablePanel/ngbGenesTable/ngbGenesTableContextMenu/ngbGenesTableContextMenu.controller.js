3246bce896118253e2bec17226e2988d7c2f7222
Dmitrii Krasnov <dmitrii_krasnov@epam.com>
2021-07-29 19:37:10 +0300

    Gene/protein annotation panel (#405):  - #issuecomment-888394297 ;  - #issuecomment-888412679 ;

--- client/client/app/components/ngbGenesTablePanel/ngbGenesTable/ngbGenesTableContextMenu/ngbGenesTableContextMenu.controller.js
+++ client/client/app/components/ngbGenesTablePanel/ngbGenesTable/ngbGenesTableContextMenu/ngbGenesTableContextMenu.controller.js
@@ -3,10 +3,28 @@ import {EventGeneInfo} from '../../../../shared/utils/events';
 export default class NgbGenesTableContextMenuController {
 
     entity = {};
+    featureName;
+    sequenceTooLong = false;
+    maxSequenceLength = Infinity;
 
-    constructor($scope, dispatcher, appLayout, projectContext, ngbGenesTableContextMenu) {
-        Object.assign(this, {dispatcher, appLayout, projectContext, ngbGenesTableContextMenu});
+    constructor($scope, dispatcher, appLayout, projectContext, ngbGenesTableContextMenu, ngbGenesTableService) {
+        Object.assign(this, {dispatcher, appLayout, projectContext, ngbGenesTableContextMenu, ngbGenesTableService});
         this.entity = $scope.row.entity;
+        this.featureName = this.entity[`${this.ngbGenesTableService.defaultPrefix}featureName`];
+
+        const blastSettings = this.projectContext.getTrackDefaultSettings('blast_settings');
+        const maxQueryLengthProperty = 'query_max_length';
+        this.maxSequenceLength = blastSettings &&
+        blastSettings.hasOwnProperty(maxQueryLengthProperty) &&
+        !Number.isNaN(Number(blastSettings[maxQueryLengthProperty]))
+            ? Number(blastSettings[maxQueryLengthProperty])
+            : Infinity;
+        const featureSize = Math.abs(
+            (this.entity[`${this.ngbGenesTableService.defaultPrefix}startIndex`] || 0)
+            - (this.entity[`${this.ngbGenesTableService.defaultPrefix}endIndex`] || 0)
+        );
+        this.sequenceTooLong = this.maxSequenceLength < featureSize;
+
     }
 
     blastSearch(tool, event) {
@@ -16,15 +34,21 @@ export default class NgbGenesTableContextMenuController {
         this.dispatcher.emitSimpleEvent('layout:item:change', {layoutChange});
         const readInfo = {
             referenceId: this.entity.referenceId,
-            chromosomeId: this.entity.chromosome.id,
-            startIndex: this.entity.start,
-            endIndex: this.entity.end
+            chromosomeId: this.entity.chromosomeObj.id,
+            startIndex: this.entity[`${this.ngbGenesTableService.defaultPrefix}startIndex`],
+            endIndex: this.entity[`${this.ngbGenesTableService.defaultPrefix}endIndex`]
         };
         const data = {
             ...readInfo,
             tool: tool,
-            source: 'geneTable'
+            source: 'gene'
         };
+        if (tool === 'blastp') {
+            data.aminoAcid = true;
+            data.feature = this.entity[`${this.ngbGenesTableService.defaultPrefix}featureType`];
+            data.name = this.entity[`${this.ngbGenesTableService.defaultPrefix}featureName`];
+            data.id = this.entity[`${this.ngbGenesTableService.defaultPrefix}featureFileId`];
+        }
         this.dispatcher.emitSimpleEvent('read:show:blast', data);
         event.stopImmediatePropagation();
     }
@@ -35,12 +59,12 @@ export default class NgbGenesTableContextMenuController {
         layoutChange.displayed = true;
         this.dispatcher.emitSimpleEvent('layout:item:change', {layoutChange});
         const data = new EventGeneInfo({
-            startIndex: this.entity.startIndex,
-            endIndex: this.entity.endIndex,
-            geneId: this.entity.featureId,
+            startIndex: this.entity[`${this.ngbGenesTableService.defaultPrefix}startIndex`],
+            endIndex: this.entity[`${this.ngbGenesTableService.defaultPrefix}endIndex`],
+            geneId: this.entity[`${this.ngbGenesTableService.defaultPrefix}featureId`],
             geneTracks: [{
-                id: this.entity.featureFileId,
-                chromosomeId: this.entity.chromosome ? +this.entity.chromosome.id : undefined
+                id: this.entity[`${this.ngbGenesTableService.defaultPrefix}featureFileId`],
+                chromosomeId: this.entity.chromosomeObj ? +this.entity.chromosomeObj.id : undefined
             }]
         });
         this.dispatcher.emitSimpleEvent('miew:show:structure', data);
