994ad7173d636bfab1335d1ca7b5d81c2425b95b
Dmitrii Krasnov <dmitrii_krasnov@epam.com>
2021-08-19 14:39:35 +0300

    Homologene support (#503): ortho & para API integration

--- client/client/app/components/ngbHomologsPanel/ngbOrthoParaResult/ngbOrthoParaResult.service.js
+++ client/client/app/components/ngbHomologsPanel/ngbOrthoParaResult/ngbOrthoParaResult.service.js
@@ -11,24 +11,11 @@ const FIRST_PAGE = 1;
 const PAGE_SIZE = 15;
 
 export default class ngbOrthoParaResultService extends ClientPaginationService {
-    _searchResultTableLoading = true;
 
     constructor(dispatcher, genomeDataService) {
         super(dispatcher, FIRST_PAGE, PAGE_SIZE, 'homologs:orthoPara:result:page:change');
         this.dispatcher = dispatcher;
         this.genomeDataService = genomeDataService;
-    }
-
-    _searchResultTableError = null;
-
-    get searchResultTableError() {
-        return this._searchResultTableError;
-    }
-
-    _orthoParaResult = {};
-
-    get orthoParaResult() {
-        return this._orthoParaResult;
     }
 
     get orthoParaResultColumns() {
@@ -62,6 +49,8 @@ export default class ngbOrthoParaResultService extends ClientPaginationService {
                     result.push({
                         cellTemplate: '<ngb-homologs-domains domains="row.entity.domainsObj"></ngb-homologs-domains>',
                         enableHiding: false,
+                        enableSorting: false,
+                        enableColumnMenu: false,
                         field: 'domainsObj',
                         headerCellTemplate: headerCells,
                         minWidth: 300,
@@ -94,65 +83,5 @@ export default class ngbOrthoParaResultService extends ClientPaginationService {
             }
         }
         return result;
-    }
-
-    async updateSearchResult(searchId) {
-        this._orthoParaResult = await this.loadOrthoParaResult(searchId);
-        this.dispatcher.emitSimpleEvent('orthoPara:result:change');
-    }
-
-    async loadOrthoParaResult(searchId) {
-        const data = await this.genomeDataService.getOrthoParaResultLoad(searchId);
-        let maxHomologLength = 0;
-        const colors = {
-            'A': 'red',
-            'B': 'blue',
-            'C': 'green',
-            'D': 'yellow',
-            'PDZ': '#F00000',
-            'PKc_like': 'pink',
-            'LIM1_LIMK1': 'lightblue',
-            'LIM2_LIMK1': 'blue',
-            'STKc_LIMK1': 'lightgreen',
-            'LIM': 'green'
-        };
-        if (data.error) {
-            this._totalPages = 0;
-            this.currentPage = FIRST_PAGE;
-            this._firstPage = FIRST_PAGE;
-            this._searchResultTableLoading = false;
-            this._searchResultTableError = data.message;
-            return [];
-        } else {
-            this._searchResultTableError = null;
-        }
-        this._totalPages = Math.ceil(data.length / this.pageSize);
-        if (data) {
-            data.forEach((value, key) => {
-                data[key] = this._formatServerToClient(value);
-                if (maxHomologLength < data[key].aa) {
-                    maxHomologLength = data[key].aa;
-                }
-            });
-            data.forEach((value, key) => {
-                data[key].domainsObj = {
-                    domains: data[key].domains.map(d => ({...d, color: colors[d.name]})),
-                    homologLength: data[key].aa,
-                    maxHomologLength: maxHomologLength
-                };
-                // delete data[key].domains;
-            });
-        }
-        return data || [];
-    }
-
-    _formatServerToClient(result) {
-        return {
-            name: result.name,
-            species: result.species,
-            aa: result.aa,
-            domains: result.domains,
-            type: result.type
-        };
     }
 }
