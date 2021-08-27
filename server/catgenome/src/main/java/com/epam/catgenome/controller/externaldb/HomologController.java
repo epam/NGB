f1d39fe008c29cd7b70fbae27f43ff8d28762578
Oksana_Kolesnikova <Oksana_Kolesnikova@epam.com>
2021-08-27 10:44:19 +0300

    Issue #522 Homologene support - NCBI gene id + search by gene id for groups and genes

--- server/catgenome/src/main/java/com/epam/catgenome/controller/externaldb/HomologController.java
+++ server/catgenome/src/main/java/com/epam/catgenome/controller/externaldb/HomologController.java
@@ -37,7 +37,11 @@ import com.wordnik.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.apache.lucene.queryparser.classic.ParseException;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 
