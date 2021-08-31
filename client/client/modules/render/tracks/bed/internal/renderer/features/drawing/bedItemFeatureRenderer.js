0667ea47a0e086f133015062f69feeaec4181891
Mikhail Rodichenko <Mikhail_Rodichenko@epam.com>
2021-08-31 19:02:23 +0300

    BED track fix

--- client/client/modules/render/tracks/bed/internal/renderer/features/drawing/bedItemFeatureRenderer.js
+++ client/client/modules/render/tracks/bed/internal/renderer/features/drawing/bedItemFeatureRenderer.js
@@ -112,6 +112,7 @@ export default class BedItemFeatureRenderer extends FeatureBaseRenderer {
         let endX = viewport.project.brushBP2pixel(feature.endIndex) + pixelsInBp / 2;
         if (
             feature.hasOwnProperty('strand') &&
+            /^(positive|negative)$/i.test(feature.strand) &&
             this.shouldRenderStrandIndicatorInsteadOfGraphics(startX, endX)
         ) {
             endX = startX +
