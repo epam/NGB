f8bf8cb9d36b9efeafb80651cc525ded668e7832
Mikhail Rodichenko <Mikhail_Rodichenko@epam.com>
2021-09-08 16:42:41 +0300

    JWT token being ignored in the client (#542) - setting 'Authorization' header for all type of methods

--- client/client/dataServices/data-service.js
+++ client/client/dataServices/data-service.js
@@ -146,6 +146,9 @@ function $http(method, url, data, config) {
         xhr.addEventListener('abort', reject);
         xhr.responseType = (config && config.customResponseType) || 'json';
         xhr.open(method, url);
+        if (token) {
+            xhr.setRequestHeader('Authorization', `Bearer ${token}`);
+        }
         switch (true) {
             case data === undefined:
                 return xhr.send();
@@ -155,7 +158,6 @@ function $http(method, url, data, config) {
                 return xhr.send(data);
             default:
                 xhr.setRequestHeader('Content-Type', 'application/json');
-                if (token) xhr.setRequestHeader('Authorization', `Bearer ${token}`);
                 return xhr.send(JSON.stringify(data));
         }
     });
