ba1cc085479657773733ccd0cb511e2a91ce7ec9
DmitriiKrasnov <80889022+DmitriiKrasnov@users.noreply.github.com>
2021-09-20 17:29:12 +0300

    Navigation from track to dataset home page (summary) (#597) (#602)

--- client/client/app/components/ngbProjectInfoPanel/ngbProjectInfoSections/ngbProjectInfoSections.controller.js
+++ client/client/app/components/ngbProjectInfoPanel/ngbProjectInfoSections/ngbProjectInfoSections.controller.js
@@ -1,16 +1,33 @@
 import baseController from '../../../shared/baseController';
 
-export default class ngbProjectInfoSectionsController extends baseController{
+export default class ngbProjectInfoSectionsController extends baseController {
     static get UID() {
         return 'ngbProjectInfoSectionsController';
     }
 
-    constructor($scope, dispatcher, ngbProjectInfoService) {
+    constructor($scope, dispatcher, ngbProjectInfoService, projectContext) {
         super(dispatcher);
         this.ngbProjectInfoService = ngbProjectInfoService;
+        this.projectContext = projectContext;
+    }
+
+    onButtonClick($mdOpenMenu, $event) {
+        if (this.projectContext.currentChromosome) {
+            this.projectContext.changeState({chromosome: null});
+        } else {
+            this.openMenu($mdOpenMenu, $event);
+        }
     }
 
     openMenu($mdOpenMenu, $event) {
+        $event.stopPropagation();
         $mdOpenMenu($event);
+    }
+
+    onItemClick(newValue, oldValue, falseValue) {
+        if (this.projectContext.currentChromosome) {
+            this.projectContext.changeState({chromosome: null});
+        }
+        this.ngbProjectInfoService.currentMode = newValue === oldValue ? falseValue : newValue;
     }
 }
