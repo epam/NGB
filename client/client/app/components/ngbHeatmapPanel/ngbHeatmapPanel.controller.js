261423f4a2a8c4cc5a70c4fd2785bfcb652733ec
Zhanna Kaymedenova <zhanna_kaymedenova@epam.com>
2021-10-13 13:02:23 +0300

    add list sorting for heatmaps

--- client/client/app/components/ngbHeatmapPanel/ngbHeatmapPanel.controller.js
+++ client/client/app/components/ngbHeatmapPanel/ngbHeatmapPanel.controller.js
@@ -1,7 +1,16 @@
+function sortByName(a, b) {
+    if (a && b && a.name && b.name) {
+        const nameA = (a.prettyName || a.name).toLowerCase();
+        const nameB = (b.prettyName || b.name).toLowerCase();
+        return nameA >  nameB ? 1 : -1;
+    }
+    return 0;
+}
 export default class ngbHeatmapPanelController {
     static get UID() {
         return 'ngbHeatmapPanelController';
     }
+    _sortedHeatMaps = {};
 
     constructor($scope, appLayout, dispatcher, heatmapContext) {
         this.appLayout = appLayout;
@@ -10,6 +19,7 @@ export default class ngbHeatmapPanelController {
         this.onHeatmapNavigationCallback = this.onHeatmapNavigation.bind(this);
         this.onHeatmapOptionsChangeCallback = this.onHeatmapOptionsChange.bind(this);
         this.heatmapContext.selectFirst();
+        this.sortedHeatMaps = this.sortHeatmaps(this.heatmaps);
         $scope.$on('$destroy', () => {
             this.heatmapContext.heatmap = undefined;
         });
@@ -23,14 +33,52 @@ export default class ngbHeatmapPanelController {
         return this.heatmapContext.heatmaps || [];
     }
 
+    sortHeatmaps(heatmaps) {
+        if (heatmaps) {
+            let selectedAnnotation, selectedTrack;
+            const formattedHeatmaps = heatmaps.reduce((sortedHeatMaps, file) => {
+                const [annotation] = this.annotationFiles.filter(item => item.id === file.id && item.format === file.format);
+                const isSelectedAnnotation = annotation && annotation.id === this.heatmapId;
+                const isSelectedTrack = !annotation && file.id === this.heatmapId;
+                const nonSelectedAnnotation = annotation && annotation.id !== this.heatmapId;
+                const nonSelectedTrack = !annotation && file.id !== this.heatmapId;
+                if (nonSelectedAnnotation) {
+                    sortedHeatMaps.annotations.push(annotation);
+                } else if (nonSelectedTrack){
+                    sortedHeatMaps.tracks.push(file);
+                } else if (isSelectedTrack){
+                    selectedTrack = file;
+                } else if (isSelectedAnnotation) {
+                    selectedAnnotation = annotation;
+                }
+                return sortedHeatMaps;
+            }, {tracks: [], annotations: []});
+            const annotations = formattedHeatmaps.annotations.sort(sortByName);
+            const tracks = formattedHeatmaps.tracks.sort(sortByName);
+            formattedHeatmaps.annotations = selectedAnnotation ? [selectedAnnotation, ...annotations] : annotations;
+            formattedHeatmaps.tracks = selectedTrack ? [selectedTrack, ...tracks] : tracks;
+            return formattedHeatmaps;
+        } else {
+            return {};
+        }
+    }
+
     get heatmap() {
         return this.heatmapContext.heatmap;
+    }
+    get sortedHeatMaps() {
+        return this._sortedHeatMaps;
+    }
+
+    set sortedHeatMaps(heatmaps) {
+        this._sortedHeatMaps = heatmaps;
     }
 
     set heatmap(heatmap) {
         this.heatmapContext.heatmap = heatmap;
         this.heatmapContext.heatmapNavigationOccurred = false;
         this.heatmapContext.refreshHeatmapsList();
+        this.sortedHeatMaps = this.sortHeatmaps(this.heatmaps);
     }
 
     get heatmapId() {
@@ -59,8 +107,20 @@ export default class ngbHeatmapPanelController {
         return undefined;
     }
 
+    get annotationFiles() {
+        if (
+            this.heatmapContext &&
+            this.heatmapContext.projectContext &&
+            this.heatmapContext.projectContext.reference
+        ) {
+            return this.heatmapContext.projectContext.reference.annotationFiles;
+        }
+        return [];
+    }
+
     onHeatmapNavigation() {
         this.heatmapContext.heatmapNavigationOccurred = true;
+        this.sortedHeatMaps = this.sortHeatmaps(this.heatmaps);
     }
 
     /**
@@ -76,6 +136,7 @@ export default class ngbHeatmapPanelController {
             Number(options.dataConfig.id) === Number(this.heatmapContext.heatmap.id)
         ) {
             this.heatmapContext.setCurrentState(options.serialize());
+            this.sortedHeatMaps = this.sortHeatmaps(this.heatmaps);
         }
     }
 }
