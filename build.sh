dda1f0f30b69c6535958737b086b610d41cb6a75
Roman Gorokhovsky <76645646+madmongoose@users.noreply.github.com>
2021-11-11 14:14:13 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Starting NGB build"
-if [[ "${GITHUB_REF##*/}" == "release/"* ]]; then
+if [[ "${GITHUB_REF##*/}" == "develop/"* ]]; then
   echo "Building with docker distribution"
   BUILD_DOCKER=buildDocker
 fi
