d7eaa1b9d03ba2d78d58b3829049c5c52d742054
Roman Gorokhovsky <76645646+madmongoose@users.noreply.github.com>
2021-11-14 11:45:05 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "Starting NGB build"
-if [[ "${GITHUB_REF##*/}" == "develop/"* ]]; then
+if [[ "${{ github.ref_name }}" == "release/"* ]]; then
   echo "Building with docker distribution"
   BUILD_DOCKER=buildDocker
 fi
 
-./gradlew buildJar buildCli buildDoc $BUILD_DOCKER -PbuildNumber=test -PnoTest
+./gradlew buildJar buildCli buildDoc $BUILD_DOCKER -PbuildNumber=${{ github.run_number }} -PnoTest
