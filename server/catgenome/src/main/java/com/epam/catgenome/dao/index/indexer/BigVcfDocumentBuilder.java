eaded922ff96fc9ec07abc746b2e802596e7d978
Oksana_Kolesnikova <Oksana_Kolesnikova@epam.com>
2021-12-10 14:01:55 +0300

    Issue #686 VCF Multi strain support

--- server/catgenome/src/main/java/com/epam/catgenome/dao/index/indexer/BigVcfDocumentBuilder.java
+++ server/catgenome/src/main/java/com/epam/catgenome/dao/index/indexer/BigVcfDocumentBuilder.java
@@ -51,7 +51,12 @@ import org.apache.lucene.util.BytesRef;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.regex.Pattern;
 
 /**
