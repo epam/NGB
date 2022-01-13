e2365035cae2c072cfef09c9c8237876ebe266ef
Dmitrii Krasnov <dmitrii_krasnov@epam.com>
2022-01-13 11:24:58 +0300

    Strain Lineage (#682): konva warnings disable

--- client/client/app/components/ngbStrainLineage/ngbCytoscape/ngbCytoscape.controller.js
+++ client/client/app/components/ngbStrainLineage/ngbCytoscape/ngbCytoscape.controller.js
@@ -48,6 +48,7 @@ export default class ngbCytoscapeController {
         Cytoscape.use(dagre);
         Cytoscape.use(dom_node);
         Cytoscape.use(contextMenus);
+        konva.showWarnings = false;
         edgeEditing( Cytoscape, $, konva );
 
         const resizeHandler = () => {
