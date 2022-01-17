90d662cb7d495fb091552169b38e0cf3fc9f9a51
Aleksandr Sidoruk <aleksandr_sidoruk@epam.com>
2022-01-17 13:12:59 +0300

    Issue #290: build NGB with postgres support automatically

--- build.sh
+++ build.sh
@@ -10,4 +10,12 @@ _build_number="${APPVEYOR_BUILD_NUMBER}"
 if [ "${APPVEYOR_REPO_COMMIT}" ]; then
   _build_number="${_build_number}.${APPVEYOR_REPO_COMMIT}"
 fi
-./gradlew buildJar buildCli buildDoc $BUILD_DOCKER -PbuildNumber=${_build_number} -PnoTest
+
+# 1. Build JAR with H2 support and "everything" else (catgenome-h2.jar)
+# 2. Rebuild JAR with PSQL support only (catgenome-psql.jar)
+# 3. H2 version is also stored as `catgenome.jar` for backward compatability, if any service uses that
+./gradlew buildJar buildCli buildDoc $BUILD_DOCKER -PbuildNumber=${_build_number} -PnoTest && \
+mv dist/catgenome.jar dist/catgenome-h2.jar && \
+./gradlew buildJar -PbuildNumber=${_build_number} -PnoTest -Pdatabase=postgres && \
+mv dist/catgenome.jar dist/catgenome-psql.jar && \
+cp dist/catgenome-h2.jar dist/catgenome.jar
