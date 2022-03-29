fb1c086f042e29c2d8336f567ece5ab45f767895
Alfiia Faizrakhmanova <Alfiia_Faizrakhmanova@epam.com>
2022-03-29 15:26:54 +0300

    removed unused isBrowserNarrow

--- client/client/app/shared/projectContext/index.js
+++ client/client/app/shared/projectContext/index.js
@@ -150,7 +150,6 @@ export default class projectContext {
 
     _lastLocalTracks = [];
     _localDataset = null;
-    _isBrowserNarrow = false;
 
     get firstPageVariations() {
         return this._firstPageVariations;
@@ -306,14 +305,6 @@ export default class projectContext {
 
     set bookmarksFilterIsDefault(value) {
         this._bookmarksFilterIsDefault = value;
-    }
-
-    get isBrowserNarrow() {
-        return this._isBrowserNarrow;
-    }
-
-    set isBrowserNarrow(value) {
-        this._isBrowserNarrow = value;
     }
 
     get vcfFilter() {
