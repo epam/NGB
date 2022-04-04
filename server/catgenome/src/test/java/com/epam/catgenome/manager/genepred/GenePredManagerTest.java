3276bed7ebf6085d675912425d18d6cd226700f1
Oksana Kolesnikova <83080415+okolesn@users.noreply.github.com>
2022-04-04 18:51:30 +0300

    Issue #798 GenePred file support (#805)
    * Issue #798 GenePred file support
    
    * Issue #798 GenePred file support
    
    * Issue #798 GenePred file support
    
    * Issue #798 GenePred file support

---
+++ server/catgenome/src/test/java/com/epam/catgenome/manager/genepred/GenePredManagerTest.java
@@ -0,0 +1,69 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2022 EPAM Systems
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.epam.catgenome.manager.genepred;
+
+import junit.framework.TestCase;
+import org.biojava.nbio.genome.parsers.gff.FeatureList;
+import org.biojava.nbio.genome.parsers.gff.GFF3Reader;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.io.File;
+import java.io.IOException;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"classpath:applicationContext-test.xml"})
+public class GenePredManagerTest extends TestCase {
+
+    @Autowired
+    private GenePredManager genePredManager;
+
+    @Autowired
+    private ApplicationContext context;
+
+    private String genePredFileName;
+    @Rule
+    public TemporaryFolder temporaryFolder = new TemporaryFolder();
+
+    @Before
+    public void setUp() throws IOException {
+        this.genePredFileName = context.getResource("classpath:genepred//cat.genepred").getFile().getPath();
+    }
+
+    @Test
+    public void createSBGNPathwayTest() throws IOException {
+        final File gffFile = temporaryFolder.newFile("cat.gff");
+        genePredManager.genePredToGTF(genePredFileName, gffFile.toPath());
+        final FeatureList featureList = GFF3Reader.read(gffFile.getAbsolutePath());
+        assertNotNull(featureList);
+        assertEquals("gene", featureList.get(0).type());
+    }
+}
