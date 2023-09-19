2b45b224845452416df26df3d04f03e3b59b7657
Alfiia Faizrakhmanova <alfiia_faizrakhmanova@epam.com>
2023-09-19 13:06:19 +0200

    added autocomplete

--- client/client/app/components/ngbTargetPanel/ngbDiseasesTab/ngbDiseasesTab.controller.js
+++ client/client/app/components/ngbTargetPanel/ngbDiseasesTab/ngbDiseasesTab.controller.js
@@ -1,10 +1,45 @@
 export default class ngbDiseasesTabController {
 
+    searchText;
+    diseaseModel;
+    diseasesList = [];
+
     static get UID() {
         return 'ngbDiseasesTabController';
     }
 
     constructor() {
         Object.assign(this, {});
+    }
+
+    getFilteredDiseases() {
+        return this.diseasesList.filter(disease => {
+            return (new RegExp(`${this.searchText}`, 'i')).test(disease);
+        });
+    }
+
+    diseaseChanged(disease) {
+        this.diseaseModel = disease;
+        this.searchText = this.diseaseModel;
+    }
+
+    searchDisease() {}
+
+    onBlur () {
+        if (this.searchText) {
+            this.diseaseChanged(this.getFilteredDiseases()[0])
+        } else {
+            this.diseaseModel = undefined;
+        }
+    }
+
+    onKeyPress (event) {
+        switch ((event.code || '').toLowerCase()) {
+            case 'enter':
+                this.onBlur();
+                break;
+            default:
+                break;
+        }
     }
 }
