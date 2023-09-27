7d7dcffff6b0c2c53945ace664f6659310c493cd
maryvictol <Mariia_Tolstaia@epam.com>
2023-09-27 22:03:50 +0300

    update run.sh

--- e2e/gui/run.sh
+++ e2e/gui/run.sh
@@ -14,6 +14,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -e
+set +e
 
 ./gradlew clean test
+
+EXIT_CODE=$?
+echo $EXIT_CODE
+exit $EXIT_CODE
