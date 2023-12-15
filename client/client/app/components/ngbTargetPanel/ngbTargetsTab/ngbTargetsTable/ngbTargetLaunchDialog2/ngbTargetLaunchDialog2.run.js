c097465dc0ca1b98604d7722f8b8ebbe56f8e42e
Alfiia Faizrakhmanova <alfiia_faizrakhmanova@epam.com>
2023-12-15 14:01:23 +0100

    added methods, confirm menu, fixed style

--- client/client/app/components/ngbTargetPanel/ngbTargetsTab/ngbTargetsTable/ngbTargetLaunchDialog2/ngbTargetLaunchDialog2.run.js
+++ client/client/app/components/ngbTargetPanel/ngbTargetsTab/ngbTargetsTable/ngbTargetLaunchDialog2/ngbTargetLaunchDialog2.run.js
@@ -7,6 +7,9 @@ export default function run(
     $mdDialog,
     $timeout,
     dispatcher,
+    ngbTargetsTabService,
+    ngbTargetPanelService,
+    targetContext,
 ) {
     const displayLaunchDialog = async (target) => {
         $mdDialog.show({
@@ -15,8 +18,11 @@ export default function run(
                 $scope.name = target.name;
                 $scope.genesOfInterest = [];
                 $scope.translationalGenes = [];
-
                 $scope.genes = groupedBySpecies(target.species.value);
+
+                $scope.identifyDisabled = () => (
+                    !$scope.genesOfInterest.length || !$scope.translationalGenes.length
+                );
 
                 function getGroupItems (item) {
                     const { genesOfInterest = [], translationalGenes = [] } = $scope;
@@ -84,16 +90,52 @@ export default function run(
                     }
                 };
 
-                $scope.identifyDisabled = () => (
-                    false
-                );
+                async function getIdentificationData(scope) {
+                    const params = {
+                        targetId: target.id,
+                        genesOfInterest: scope.genesOfInterest.map(s => s.geneId),
+                        translationalGenes: scope.translationalGenes.map(s => s.geneId)
+                    };
+                    const info = {
+                        target: target,
+                        interest: scope.genesOfInterest,
+                        translational: scope.translationalGenes
+                    };
+                    const result = await ngbTargetsTabService.getIdentificationData(params, info);
+                    if (result) {
+                        dispatcher.emit('target:show:identification:tab');
+                        targetContext.setCurrentIdentification(target, scope);
+                    }
+                }
 
-                $scope.identify = () => console.log('identify');
+                $scope.identify = () => {
+                    if (ngbTargetPanelService.identificationData && ngbTargetPanelService.identificationTarget) {
+                        const self = $scope;
+                        $mdDialog.show({
+                            template: require('./ngbTargetLaunchConfirmDialog.tpl.html'),
+                            controller: function($scope, $mdDialog) {
+                                $scope.launch = function () {
+                                    getIdentificationData(self);
+                                    $mdDialog.hide();
+                                    $mdDialog.hide();
+                                };
+                                $scope.cancel = function () {
+                                    $mdDialog.hide();
+                                };
+                            },
+                            preserveScope: true,
+                            autoWrap: true,
+                            skipHide: true,
+                        });
+                        return;
+                    }
+                    getIdentificationData($scope);
+                    $mdDialog.hide();
+                };
 
                 $scope.close = () => {
                     $mdDialog.hide();
                 };
-                
             },
             clickOutsideToClose: true
         });
