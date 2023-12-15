9a30eb5490b945ad6c62b38409c80ed05db7e8e1
Alfiia Faizrakhmanova <alfiia_faizrakhmanova@epam.com>
2023-12-15 13:58:53 +0100

    added dropdown filtered by input text

--- client/client/app/components/ngbTargetPanel/ngbTargetsTab/ngbTargetsTable/ngbTargetLaunchDialog2/ngbLaunchMenuInput.directive.js
+++ client/client/app/components/ngbTargetPanel/ngbTargetsTab/ngbTargetsTable/ngbTargetLaunchDialog2/ngbLaunchMenuInput.directive.js
@@ -9,9 +9,71 @@ ngbLaunchMenuInput.directive('ngbLaunchMenuInput', function() {
         restrict: 'E',
         scope: {
           label: '@',
+          ngModel: '=',
+          getGenesList: '=',
         },
         template: require('./ngbLaunchMenuInput.tpl.html'),
-        controller: function($scope) {}
+        controller: function($scope, $element) {
+            $scope.listIsDisplayed = false;
+            $scope.hideListTimeout = null;
+            $scope._hideListIsPrevented = false;
+            $scope.input = $element;
+
+            $scope.preventListFromClosing = () => {
+                $scope._hideListIsPrevented = true;
+                $scope.input.focus();
+            }
+
+            $scope.stopPreventListFromClosing = () => {
+                $scope._hideListIsPrevented = false;
+            }
+
+            $scope.hideListDelayed = () => {
+                if ($scope.hideListTimeout) {
+                    clearTimeout($scope.hideListTimeout);
+                    $scope.hideListTimeout = null;
+                }
+                $scope.hideListTimeout = setTimeout(() => {
+                    $scope.hideList();
+                }, 100);
+            }
+
+            $scope.hideList = () => {
+                if ($scope.hideListTimeout) {
+                    clearTimeout($scope.hideListTimeout);
+                    $scope.hideListTimeout = null;
+                }
+                if ($scope._hideListIsPrevented) {
+                    return;
+                }
+                $scope.listIsDisplayed = false;
+                $scope.apply();
+                $scope.$apply();
+            }
+
+            $scope.displayList = () => {
+                if ($scope.hideListTimeout) {
+                    clearTimeout($scope.hideListTimeout);
+                    $scope.hideListTimeout = null;
+                }
+                $scope.listElements = $scope.getGenesList();
+                const input = $scope.input[0];
+                const top = input.offsetTop + input.clientHeight - 8;
+                $scope.listPosition = {
+                    top: `${top}px`,
+                    left: `${170}px`
+                }
+                $scope.listIsDisplayed = true;
+            }
+
+            $scope.onChange = (text) => {
+                $scope.listElements = $scope.getGenesList(text);
+            }
+
+            $scope.apply = () => {
+                $scope.ngModel = '';
+            }
+        }
     };
 });
 
--- client/client/app/components/ngbTargetPanel/ngbTargetsTab/ngbTargetsTable/ngbTargetLaunchDialog2/ngbTargetLaunchDialog2.run.js
+++ client/client/app/components/ngbTargetPanel/ngbTargetsTab/ngbTargetsTable/ngbTargetLaunchDialog2/ngbTargetLaunchDialog2.run.js
@@ -1,3 +1,8 @@
+import {
+    groupedBySpecies,
+    createFilterFor,
+} from '../utilities/autocompleteFunctions';
+
 export default function run(
     $mdDialog,
     $timeout,
@@ -8,6 +13,41 @@ export default function run(
             template: require('./ngbTargetLaunchDialog2.tpl.html'),
             controller: function ($scope) {
                 $scope.name = target.name;
+                $scope.searchText = '';
+                $scope.interestModel = '';
+                $scope.translationalModel = '';
+                $scope.genesOfInterest = [];
+                $scope.translationalGenes = [];
+
+                $scope.genes = groupedBySpecies(target.species.value);
+
+                function getGroupItems (item) {
+                    const { genesOfInterest = [], translationalGenes = [] } = $scope;
+                    return $scope.genes
+                        .filter(s => s.item
+                            && s.speciesName === item.speciesName
+                            && !genesOfInterest.includes(s)
+                            && !translationalGenes.includes(s));
+                }
+
+                function filterGroupHead() {
+                    return (gene) => {
+                        if (gene.group) {
+                            const items = getGroupItems(gene);
+                            return items.length;
+                        }
+                        return true;
+                    };
+                }
+
+                $scope.getFilteredGenes = (text) => {
+                    const genes = $scope.genes
+                        .filter(s => !$scope.genesOfInterest.includes(s))
+                        .filter(s => !$scope.translationalGenes.includes(s))
+                        .filter(filterGroupHead());
+                    if (!text) return genes;
+                    return genes.filter(createFilterFor(text));
+                }
 
                 $scope.identifyDisabled = () => (
                     false
