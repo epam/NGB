d9d2ab294abb4aaeed678bb06a434dac2ee88f76
Alfiia Faizrakhmanova <alfiia_faizrakhmanova@epam.com>
2024-02-28 18:10:48 +0100

    renamed target gene table component

--- client/client/app/components/ngbTargetPanel/ngbTargetsTab/ngbTargetsForm/ngbTargetGenesTable/ngbGenesTableFilter/ngbGenesTableFilter.component.js
+++
@@ -1,9 +0,0 @@
-import controller from './ngbGenesTableFilter.controller';
-
-export default  {
-    bindings: {
-        column: '<'
-    },
-    controller: controller.UID,
-    template: require('./ngbGenesTableFilter.tpl.html')
-};
