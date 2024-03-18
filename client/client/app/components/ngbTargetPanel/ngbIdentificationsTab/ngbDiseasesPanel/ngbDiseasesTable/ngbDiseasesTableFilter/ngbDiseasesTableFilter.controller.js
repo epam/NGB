83bfe649b9892f6fb2845837911a770a53d50775
Alfiya <47081183+AlfiyaRF@users.noreply.github.com>
2024-03-18 12:31:48 +0100

    changed drugs and diseases filtering for target column for ttd (#1193)

--- client/client/app/components/ngbTargetPanel/ngbIdentificationsTab/ngbDiseasesPanel/ngbDiseasesTable/ngbDiseasesTableFilter/ngbDiseasesTableFilter.controller.js
+++ client/client/app/components/ngbTargetPanel/ngbIdentificationsTab/ngbDiseasesPanel/ngbDiseasesTable/ngbDiseasesTableFilter/ngbDiseasesTableFilter.controller.js
@@ -17,6 +17,10 @@ export default class ngbDiseasesTableFilterController {
                 this.isList = true;
                 break;
             }
+            case 'TTD target': {
+                this.isList = true;
+                break;
+            }
             default:
                 this.isString = true;
                 break;
